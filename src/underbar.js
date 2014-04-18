68f308dba0b8ad01f44c9fc8c2935fc68325862d
Bob Owen <bob@bobowen.org>
2014-04-18 08:31:26 -0400

    Completed _.default function

--- src/underbar.js
+++ src/underbar.js
@@ -255,6 +255,16 @@ var _ = {};
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function (objFrom){
+      if(!(objFrom == obj)) {
+        _.each(objFrom, function (val, key) {
+          if(!(key in obj)) {
+            obj[key] = objFrom[key];
+          }
+        });
+      }
+    });
+    return obj;
   };
 
 
